3e3242297a9963bc57aa9ca4227915d1331d8490
donkomura <koiru3822fs@gmail.com>
2022-08-22 23:10:32 +0900

    Chapter 7 : highlighting number/filetype

--- src/document.rs
+++ src/document.rs
@@ -1,3 +1,4 @@
+use crate::FileType;
 use crate::Position;
 use crate::Row;
 use crate::SearchDirection;
@@ -9,22 +10,28 @@ pub struct Document {
     rows: Vec<Row>,
     pub file_name: Option<String>,
     dirty: bool,
+    file_type: FileType,
 }
 
 impl Document {
     pub fn open(filename: &str) -> Result<Self, std::io::Error> {
         let contents = fs::read_to_string(filename)?;
+        let file_type = FileType::from(filename);
         let mut rows = Vec::new();
         for value in contents.lines() {
             let mut row = Row::from(value);
-            row.highlight();
+            row.highlight(file_type.highlighting_options(), None);
             rows.push(row);
         }
         Ok(Self {
             rows,
             file_name: Some(filename.to_string()),
             dirty: false,
+            file_type: file_type,
         })
+    }
+    pub fn file_type(&self) -> String {
+        self.file_type.name()
     }
     pub fn row(&self, index: usize) -> Option<&Row> {
         self.rows.get(index)
@@ -47,8 +54,8 @@ impl Document {
         #[allow(clippy::indexing_slicing)]
         let current_row = &mut self.rows[at.y];
         let mut new_row = current_row.split(at.x);
-        current_row.highlight();
-        new_row.highlight();
+        current_row.highlight(self.file_type.highlighting_options(), None);
+        new_row.highlight(self.file_type.highlighting_options(), None);
         #[allow(clippy::integer_arithmetic)]
         self.rows.insert(at.y + 1, new_row);
     }
@@ -82,20 +89,23 @@ impl Document {
             let next_row = self.rows.remove(at.y + 1);
             let row = &mut self.rows[at.y];
             row.append(&next_row);
-            row.highlight();
+            row.highlight(self.file_type.highlighting_options(), None);
         } else {
             let row = &mut self.rows[at.y];
             row.delete(at.x);
-            row.highlight();
+            row.highlight(self.file_type.highlighting_options(), None);
         }
     }
     pub fn save(&mut self) -> Result<(), Error> {
         if let Some(file_name) = &self.file_name {
             let mut file = fs::File::create(file_name)?;
-            for row in &self.rows {
+            self.file_type = FileType::from(file_name);
+            for row in &mut self.rows {
                 file.write_all(row.as_bytes())?;
                 file.write_all(b"\n")?;
+                row.highlight(self.file_type.highlighting_options(), None);
             }
+            self.file_type = FileType::from(file_name);
             self.dirty = false;
         }
         Ok(())
@@ -138,5 +148,11 @@ impl Document {
             }
         }
         None
+    }
+
+    pub fn highlight(&mut self, word: Option<&str>) {
+        for row in &mut self.rows {
+            row.highlight(self.file_type.highlighting_options(), word);
+        }
     }
 }
--- src/editor.rs
+++ src/editor.rs
@@ -151,6 +151,7 @@ impl Editor {
                     } else if moved {
                         editor.move_cursor(Key::Left);
                     }
+                    editor.document.highlight(Some(query));;;;
                 },
             )
             .unwrap_or(None);
@@ -159,6 +160,7 @@ impl Editor {
             self.cursor_position = old_position;
             self.scroll();
         }
+        self.document.highlight(None);
     }
 
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
@@ -347,7 +349,8 @@ impl Editor {
             modified_indicator,
         );
         let line_indicator = format!(
-            "{}/{}",
+            "{} | {}/{}",
+            self.document.file_type(),
             self.cursor_position.y.saturating_add(1),
             self.document.len()
         );
